1953b0fa0c9341affac94e91ecc454c499cc8095
git <git@git-scm.com>
2022-03-18 17:41:27 +0700

    Update

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ grep -qxF 'deb https://poomsmart.github.io/repo/ ./' /var/mobile/Library/Caches/
 grep -qxF 'deb https://repo.packix.com/ ./' /var/mobile/Library/Caches/com.saurik.Cydia/sources.list || echo 'deb https://repo.packix.com/ ./' >> /var/mobile/Library/Caches/com.saurik.Cydia/sources.list
 
 apt update
-apt install -y python3 screen curl wget vim sqlite3 rsync com.tigisoftware.appdatamanager com.tigisoftware.filza ws.hbang.newterm2
+apt install -y openssh python3 screen curl wget vim sqlite3 rsync com.tigisoftware.appdatamanager com.tigisoftware.filza ws.hbang.newterm2
 apt install -y --allow-unauthenticated com.google.ios.ytadblock com.ps.ytautofullscreen jp.soh.reprovision
 python3 -m ensurepip --upgrade
 pip3 install requests
